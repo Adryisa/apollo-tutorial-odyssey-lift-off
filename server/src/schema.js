f38f30f5917599957634df44036fc00b527ea8e5
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2022-02-24 13:50:04 +0100

    Add typeDefs track and author

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,1 +1,22 @@
 const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    modulesCount: Int
+  }
+
+  "Author of a complete track or a Module"
+  type Author {
+    id: ID!
+    name: String!
+    photo: String
+  }
+`;
+
+module.exports = typeDefs;

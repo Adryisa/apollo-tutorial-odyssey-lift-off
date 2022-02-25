478325ea21b8b7d56cd73db5b2f1677b1f531d6a
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2022-02-25 10:34:43 +0100

    Add data results by apollo client for rendering

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Layout } from "../components";
 import { gql, useQuery } from "@apollo/client";
-
+import TrackCard from "../containers/track-card";
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
@@ -25,7 +25,21 @@ export const TRACKS = gql`
 `;
 
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+  if (loading) {
+    return "Loading...";
+  }
+
+  if (error) {
+    return `Error! ${error.message}`;
+  }
+  return (
+    <Layout grid>
+      {data?.tracksForHome?.map((track) => (
+        <TrackCard key={track.id} track={track} />
+      ))}
+    </Layout>
+  );
 };
 
 export default Tracks;

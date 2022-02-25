2307e428769dc03911ce7b9b6e56d24675ebe871
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2022-02-25 10:19:09 +0100

    Add query tracks on tracks.js

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { Layout } from "../components";
+import { gql, useQuery } from "@apollo/client";
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+
+/** TRACKS query to retrieve all tracks */
+export const TRACKS = gql`
+  query Query {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      modulesCount
+      length
+      author {
+        photo
+        name
+      }
+    }
+  }
+`;
+
 const Tracks = () => {
   return <Layout grid> </Layout>;
 };
